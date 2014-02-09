3644045ba57c976f5aaba4df0ae93a94d961cd19
abusalam <abusalamparvezalam@gmail.com>
2014-02-09 18:13:40 +0530

    Persistent Storage and Retrieval of AppIDs and Status Display

--- HackBCWD.user.js
+++ HackBCWD.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name          HackBCWD
-// @namespace     https://github.com/abusalam/HackBCWD/
+// @namespace     https://github.com/abusalam/HackBCWD
 // @description   Auto Approval for SDO: http://castcertificatewb.gov.in/
 // @include       http://castcertificatewb.gov.in/*
 // @grant         GM_getValue
 // @grant         GM_setValue
+// @grant         GM_deleteValue
+// @grant         GM_listValues
 // @version       1.0
-// @icon          http://www.example.net/icon.png
+// @icon          https://github.com/abusalam
 // ==/UserScript==
-
 
 function CheckActive() {
   if (jQ('#Login').is(":enabled")) {
@@ -40,6 +41,61 @@ function addJQuery(callback) {
             + callback.toString() + ")();";
     document.body.appendChild(script);
     CheckActive();
+
+    jQ(function() {
+
+      jQ("#CmdAppIDs").click(function() {
+        console.log(GM_listValues());
+        var vals = [];
+        jQ.each(GM_listValues(), function(index, value) {
+          vals.push(GM_getValue(value));
+          //GM_deleteValue(value);
+        });
+        if (jQ("#AppIDs").val().length === 0) {
+          jQ("#AppIDs").val(vals.join(","));
+        }
+        var AppIndex = 1;
+        var AppIDs = "" + jQ("#AppIDs").val();
+        var AllIDs = AppIDs.replace(/\r\n+|\r+|\n+|\s+/gm, '').split(",");
+        jQ.each(AllIDs, function(index, value) {
+          if (value.length > 0) {
+            alert("Index: " + AppIndex + " = " + value);
+            GM_setValue("AppID_" + AppIndex++, value);
+          }
+        });
+      });
+
+      jQ("#CmdStatus").click(function() {
+
+        if (jQ("#CmdStatus").val() === "Show Status") {
+
+          jQ("#CmdStatus").val("Hide Status");
+          jQ("#AppIDs").hide();
+
+          var vals = [];
+          jQ.each(GM_listValues(), function(index, value) {
+            vals.push(GM_getValue(value));
+          });
+
+          if (jQ("#AppIDs").val().length === 0) {
+            jQ("#AppIDs").val(vals.join(","));
+          }
+          var StatusDiv = document.createElement("div");
+          StatusDiv.setAttribute("id", "AppStatus");
+
+          jQ("#appid").parent().append(StatusDiv);
+
+          jQ("#AppStatus").html("<ol><li>" + vals.join("</li><li>") + "</li></ol>");
+        } else {
+
+          jQ("#AppStatus").remove();
+          jQ("#AppIDs").show();
+          jQ("#CmdStatus").val("Show Status");
+        }
+
+      });
+
+    });
   }, false);
   document.body.appendChild(script);
 }
@@ -56,7 +112,18 @@ function main() {
   jQ("#pwd").val("<Password>").blur();
 
   jQ("#appid").val("<AppID>").blur();
+  jQ("#process font").html("Please enter all the Application IDs separated"
+          + " by comma: ','");
+  var formElem = '<textarea id="AppIDs" rows="20" cols="60"></textarea><br/>'
+          + '<input type="button" id="CmdAppIDs" value="Start Processing"/>'
+          + '<input type="button" id="CmdStatus" value="Show Status"/>';
 
+  jQ("#appid").parent().append(formElem);
+
+  jQ('[src$="login.jsp"]').load(function() {
+    console.log("Adding My Content: " + jQ("div").length);
+    //.html("<span>My Content</span>");
+  });
 }
 
 // load jQuery and execute the main function
